7b75768153f38b2b9a7c5aeee6ea1b2c3e43fb16
leph83 <info@phuc.at>
2021-06-05 20:40:13 +0200

    ajusted media text block

--- inc/load_css.php
+++ inc/load_css.php
@@ -6,11 +6,17 @@ if (!defined('ABSPATH')) {
 if (!function_exists('wtp_phuctenberg_load_css')) {
     function wtp_phuctenberg_load_css()
     {
-        $css_file = 'assets/css/phuctenberg.css';
+        $css_path = 'assets/css/';
+        $css_files = array(
+            'phuctenberg.css',
+            'media-text.css',
+        );
 
         // STYLES
-        $css_version = filemtime(plugin_dir_path(dirname(__FILE__)) . $css_file);
-        wp_enqueue_style('wtp-phuctenberg', plugin_dir_url(dirname(__FILE__)) . $css_file, false, $css_version);
+        foreach ($css_files as $key => $css_file) {
+            $css_version = filemtime(plugin_dir_path(dirname(__FILE__)) . $css_path . $css_file);
+            wp_enqueue_style('wtp-phuctenberg-' . $key, plugin_dir_url(dirname(__FILE__)) . $css_path. $css_file, false, $css_version);
+        }
     }
     add_action('wp_enqueue_scripts', 'wtp_phuctenberg_load_css', 99);
 }
