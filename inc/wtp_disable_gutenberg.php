6152e5ed88e08fd2ed0d17e5ea19fe709502da11
leph83 <info@phuc.at>
2022-11-02 09:49:13 +0100

    added stuff i need

--- inc/wtp_disable_gutenberg.php
+++ inc/wtp_disable_gutenberg.php
@@ -3,8 +3,6 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-
-
 /*
 ** REMOVE GUTENBERG STYLE FRONTEND
 */
@@ -17,3 +15,40 @@ if (!function_exists('wtp_remove_wp_block_library_css')) {
     }
     add_action('wp_enqueue_scripts', 'wtp_remove_wp_block_library_css', 100);
 }
+
+
+
+
+
+
+// REMOVE GUTENBERG STYLES FROM FRONTEND
+function pse_remove_core_block_styles()
+{
+	wp_dequeue_style('wp-block-media-text');
+}
+add_action('wp_enqueue_scripts', 'pse_remove_core_block_styles');
+
+// REMOVE ALL GUTENBERG STYLES FROM BACKEND
+add_action(
+	'wp_default_styles',
+	function ($styles) {
+
+		/* Create an array with the two handles wp-block-library and
+		 * wp-block-library-theme.
+		 */
+		$handles = ['wp-block-library', 'wp-block-library-theme'];
+
+		foreach ($handles as $handle) {
+			// Search and compare with the list of registered style handles:
+			$style = $styles->query($handle, 'registered');
+			if (!$style) {
+				continue;
+			}
+			// Remove the style
+			$styles->remove($handle);
+			// Remove path and dependencies
+			$styles->add($handle, false, []);
+		}
+	},
+	PHP_INT_MAX
+);
